718c440318c9c467014d934709a3525d752e4290
Leo Sin <lfsin3-c@my.cityu.edu.hk>
2019-11-30 04:36:50 +0800

    correct document frequency

--- Makefile
+++ Makefile
@@ -12,8 +12,9 @@ MAIN_SCRIPT = main.scala
 GENERATE_CHART_SCRIPT = generate_chart.R
 
 OUTPUTS_BASE = overall $(shell for i in `seq 5`; do echo "rating_$$i"; done)
-OUTPUTS_BASENAME = $(patsubst %, %.terms, $(OUTPUTS_BASE)) # $(patsubst %, %.df, $(OUTPUTS_BASE))
-OUTPUTS_FILENAME = $(addprefix $(OUTPUT_DIR)/, $(patsubst %, %.$(OUTPUT_EXTENSION), $(OUTPUTS_BASENAME)))
+OUTPUTS_BASENAME = $(patsubst %, %.terms, $(OUTPUTS_BASE)) $(patsubst %, %.tf, $(OUTPUTS_BASE)) $(patsubst %, %.tfidf, $(OUTPUTS_BASE)) # $(patsubst %, %.documents, $(OUTPUTS_BASE))
+OUTPUTS_FILENAME = $(addprefix $(OUTPUT_DIR)/, $(patsubst %, %.$(OUTPUT_EXTENSION), $(OUTPUTS_BASENAME))) $(addprefix $(OUTPUT_DIR)/, $(patsubst %, %.json, $(patsubst %, %.documents, $(OUTPUTS_BASE))))
+# OUTPUTS_FILENAME = $(addprefix $(OUTPUT_DIR)/, $(patsubst %, %.$(OUTPUT_EXTENSION), $(patsubst %, %.terms, $(OUTPUTS_BASE))) $(patsubst %, %.json, $(patsubst %, %.documents, $(OUTPUTS_BASE))))
 CHARTS_FILENAME = $(addprefix $(CHARTS_DIR)/, $(patsubst %, %.png, $(OUTPUTS_BASE)))
 
 all: setup main outputs charts
@@ -34,9 +35,14 @@ outputs: $(OUTPUTS_FILENAME)
 $(OUTPUT_DIR)/%.$(OUTPUT_EXTENSION):
 	@$(CMD_HDFS_MERGEPARTS) $(HDFS_PROJECT_DIR)/$(notdir $@) $@
 
+$(OUTPUT_DIR)/%.json:
+	@$(CMD_HDFS_MERGEPARTS) $(HDFS_PROJECT_DIR)/$(notdir $@) $@
+
 charts: $(CHARTS_FILENAME)
 
-$(CHARTS_DIR)/%.png: $(OUTPUT_DIR)/%.terms.$(OUTPUT_EXTENSION) # $(OUTPUT_DIR)/%.df.$(OUTPUT_EXTENSION)
+$(CHARTS_DIR)/%.png: $(OUTPUT_DIR)/%.terms.$(OUTPUT_EXTENSION) $(OUTPUT_DIR)/%.tf.$(OUTPUT_EXTENSION) $(OUTPUT_DIR)/%.tfidf.$(OUTPUT_EXTENSION)
+	# $(OUTPUT_DIR)/%.documents.json
+	# $(OUTPUT_EXTENSION)
 	@$(R) $(GENERATE_CHART_SCRIPT) $^
 
 # $(CHARTS_DIR)/%.png: 
--- main.scala
+++ main.scala
@@ -1,80 +1,88 @@
 import org.apache.spark.ml.{Pipeline, PipelineModel, PipelineStage}
 import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.feature.{CountVectorizer, CountVectorizerModel, Tokenizer, RegexTokenizer, StopWordsRemover, IDF}
-import org.apache.spark.ml.linalg.{Vector, DenseVector}
+import org.apache.spark.ml.feature.{CountVectorizer, CountVectorizerModel, Tokenizer, RegexTokenizer, StopWordsRemover, IDF, IDFModel}
+import org.apache.spark.ml.linalg.{Vector, DenseVector, SparseVector, Matrix, Matrices, SparseMatrix}
+// import org.apache.spark.ml.linalg.distributed.RowMatrix
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, Row, Column}
 import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
+import org.apache.spark.ml.clustering.LDA
 import spark.implicits._
 
 def json_to_df(path: String, columns: Array[String] = Array("*")): DataFrame = {
   val df = spark.read.json(path)
-  return df.select(columns.head, columns.tail: _*)
+  return df.select(columns.head, columns.tail: _*).withColumn("document_id", monotonically_increasing_id())
 }
-
-// val schema = StructType(Seq(
-//     StructField("indices", ArrayType(LongType, true), true), 
-//     StructField("size", LongType, true),
-//     StructField("type", ShortType, true), 
-//     StructField("values", ArrayType(DoubleType, true), true)
-// ))
-// 
-// val tf_df_schema = StructType(Seq(StructField("tf_df", schema, true)))
-// val tfidf_df_schema = StructType(Seq(StructField("tfidf_df", schema, true)))
-
-// val getitem = ((column: Column, member: String) => from_json(to_json(struct(column)), schema).getItem(member))
-
-// case class Intermediate(_type: ByteType, size: Int, indices: Array[Int], values: Array[Double]) // extends UserDefinedType
-
-// val get_indices = udf((xs: Intermediate) => xs.indices)
-// val get_values = udf((xs: Intermediate) => xs.values)
 
 val toDense = udf((xs: Vector) => xs.toDense)
 def vectorsum(a: Array[Double], b: Array[Double]): Array[Double] = {
   return (a, b).zipped.map(_ + _)
 }
 
+val vector2array = udf((v: SparseVector) => v.toArray)
+
 val agg_vectorsum = (df: DataFrame, column: Column) => df.select(column).rdd.map { case Row(v: Vector) => v.toDense.toArray }.reduce(vectorsum)
 
-// User Defined Aggregate Function
-def transform(df: DataFrame, inputColumn: String = "reviewText"): (DataFrame, DataFrame) = {
+val activeEntries = udf((v: SparseVector) => v.numActives)
+
+/*
+def sparsevectorrdd_to_sparsematrix(vectors: Array[SparseVector], columns: Array[String]): SparseMatrix = {
+  val (colptrs: Array[Array[Int]], rowindices: Array[Array[Int]], values: Array[Array[Double]]) = vectors.zipWithIndex.map { case (v: SparseVector, i: Int) => (Array(v.indices.length), v.indices, v.values) }.unzip3
+  val colPtrs: Array[Int] = (Array(0) ++ (colptrs.flatten[Int])).map{ var acc = 0; value => {acc += value; acc} }
+  return new SparseMatrix(vectors.length, columns.size, colPtrs, rowindices.flatten, values.flatten, true)
+}
+*/
+
+def vectorColToDF(df: DataFrame, column_name: String, header: Array[String]): DataFrame = {
+  val df_ = df.withColumn("tmp", vector2array(col(column_name)))
+  val sqlexpr = header.zipWithIndex.map { case (alias, index) => $"tmp".getItem(index).as(alias) }
+  return df_.select(sqlexpr : _*)
+}
+
+def transform(df: DataFrame, inputColumn: String = "reviewText", idColumn: String = "id"): (DataFrame, DataFrame, Array[String]) = { // Matrix, Matrix) = {
   val tokenizer = new RegexTokenizer().setInputCol(inputColumn).setOutputCol("token").setPattern("\\W")
   val stopwordsremover = new StopWordsRemover().setInputCol("token").setOutputCol("words")
-  // val countvectorizer = new CountVectorizer().setInputCol("words").setOutputCol("tf_df")
-  // val idf = new IDF().setInputCol("tf_df").setOutputCol("tfidf_df")
   val countvectorizer = new CountVectorizer().setInputCol("words").setOutputCol("tf")
   val idf = new IDF().setInputCol("tf").setOutputCol("tfidf")
+  val lda = new LDA().setK(10).setMaxIter(10).setFeaturesCol("tfidf").setTopicDistributionCol("lda")
 
-  val stages: Array[PipelineStage] = Array(tokenizer, stopwordsremover, countvectorizer, idf)
-
+  val stages: Array[PipelineStage] = Array(tokenizer, stopwordsremover, countvectorizer, idf, lda)
   val pipeline = new Pipeline().setStages(stages)
   val model = pipeline.fit(df)
 
   val terms_model: CountVectorizerModel = model.stages(stages.indexOf(countvectorizer)).asInstanceOf[CountVectorizerModel]
+  val idf_model: IDFModel = model.stages(stages.indexOf(idf)).asInstanceOf[IDFModel]
 
-  // val transformed = model.transform(df)
+  // val df_by_documents = model.transform(df).select($"document_id", $"tf", $"tfidf", $"lda")
+  val df_by_documents = model.transform(df).select($"document_id", $"tf", $"tfidf", activeEntries($"tf") as "n", $"lda")
 
-  // val outputdf = transformed.select(from_json(to_json(struct($"tf_df")), tf_df_schema).getItem("tf_df").getItem("indices") as "indices", from_json(to_json(struct($"tf_df")), tf_df_schema).getItem("tf_df").getItem("values") as "tf", from_json(to_json(struct($"tfidf_df")), tfidf_df_schema).getItem("tfidf_df").getItem("values") as "tfidf")
-  // outputdf.show(truncate=true)
+  val df_count = df.count()
 
-  // return (Seq((terms_model.vocabulary)).toDF("terms"), outputdf)
-  val outputdf = model.transform(df).select("tf", "tfidf")
+  val terms = terms_model.vocabulary
 
-  val corpusdf = (terms_model.vocabulary, agg_vectorsum(outputdf, $"tf"), agg_vectorsum(outputdf, $"tfidf")).zipped.toSeq.toDF("term", "tf", "tfidf") // .select($"term", outputdf.agg(sum($"tf")) as "tf", outputdf.agg(sum($"tfidf")) as "tfidf")
-  // outputdf.agg(sum($"tfidf" / $"tf")), outputdf.
+  // val tf: Matrix = sparsevectorrdd_to_sparsematrix(df_by_documents.select("tf").rdd.map { case Row(v: SparseVector) => v }.collect, terms)
+  // val tfidf: Matrix = sparsevectorrdd_to_sparsematrix(df_by_documents.select("tfidf").rdd.map { case Row(v: SparseVector) => v }.collect, terms)
 
-  return (corpusdf, outputdf)
+  val log_D_plus1 = math.log(df_count + 1)
+
+  val df_by_terms: DataFrame = (terms, idf_model.idf.toArray).zipped.toSeq.toDF("term", "idf").withColumn("df", round(exp(-$"idf" + lit(log_D_plus1)) - lit(1)))
+
+  return (df_by_terms, df_by_documents, terms) 
 }
 
-
 val dataframe = json_to_df("musical.json", columns = Array("reviewText", "overall"))
-val rating_dfs: Map[Any, (DataFrame, DataFrame)] = List(1, 2, 3, 4, 5, "overall").map(rating => (rating, transform(if (rating.isInstanceOf[String]) dataframe else dataframe.where($"overall" === rating)))).toMap
+val rating_dfs: Map[Any, (DataFrame, DataFrame, Array[String])] = List(1, 2, 3, 4, 5, "overall").map(rating => (rating, transform(if (rating.isInstanceOf[String]) dataframe else dataframe.where($"overall" === rating)))).toMap
 
-for ((rating, (terms: DataFrame, df: DataFrame)) <- rating_dfs) {
+for ((rating, (df_by_terms: DataFrame, df_by_documents: DataFrame, terms: Array[String])) <- rating_dfs) {
   val basename: String = "project/" + (if (rating.isInstanceOf[String]) "overall" else s"rating_$rating")
-  terms.repartition(1).write.mode("overwrite").option("header", "true").format("csv").save(s"$basename.terms.csv")
-  df.repartition(1).write.mode("overwrite").format("json").save(s"$basename.df.json")
+  df_by_terms.write.mode("overwrite").option("header", "true").format("csv").save(s"$basename.terms.csv")
+  df_by_documents.write.mode("overwrite").format("json").save(s"$basename.documents.json")
+
+  vectorColToDF(df_by_documents, "tf", terms).write.mode("overwrite").option("header", "true").format("csv").save(s"$basename.tf.csv")
+  vectorColToDF(df_by_documents, "tfidf", terms).write.mode("overwrite").option("header", "true").format("csv").save(s"$basename.tfidf.csv")
+
 }
 
 // Exit the program
